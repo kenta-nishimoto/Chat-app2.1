cef68023cea31ee5c4b32f14e0b8bf5038b4cd04
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-11-02 12:35:19 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validatas :name, presence: true
+  # ↪︎nameが空だと保存できないように設定している
 end
