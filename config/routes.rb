5be58bcbbc0ac389bf7175170ad1a5d8be2697c7
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-11-02 12:25:22 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
+  # ↪︎このルーティングはユーザー情報の編集へ繋がる
 end
