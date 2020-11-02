f8cc40b2eb250149e7b8ee9e96468bae45576e16
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-11-02 12:13:10 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # authenticate_user!：deviseに入っているメソッドでログインしていないと自動でログイン画面に遷移される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # devise_parameter_sanitizer：deviseのメソッドでカラムの許可を行う
+    # 上記の記述のおかげでnameカラムの保存が可能になっている
+  end
 end
