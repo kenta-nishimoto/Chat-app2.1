12fceb47193e0dc4398bccf38af174729840d973
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-11-02 14:57:29 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: "messages#index"
   resources :users, only: [:edit, :update]
   # ↪︎このルーティングはユーザー情報の編集へ繋がる
+  resources :rooms, only: [:new, :create]
+  # ↪︎ルームの新規作成のためのルーティング
 end
