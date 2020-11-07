c24be9db2e84622ef9f1ad4b69d1ee6231a3c207
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-11-07 11:08:22 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+  # ↪︎メッセージテーブルと画像を紐づけるための記述
+  # has_one_attachedメソッド：各レコードとファイルを１対１の関係で結ぶ
 
   validates :content, presence: true
 end
